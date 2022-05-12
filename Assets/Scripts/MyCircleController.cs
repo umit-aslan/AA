1a49ace16d700af7957ed29ca1a67b2e6b3c8e4c
Ümit Aslan <prototprotot2@gmail.com>
2022-05-12 11:32:28 +0300

    Circle being rotated.

---
+++ Assets/Scripts/MyCircleController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCircleController : MonoBehaviour
+{   
+    [SerializeField]float rotateSpeed;
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        transform.Rotate(0,0,rotateSpeed*Time.deltaTime);
+    }
+}
